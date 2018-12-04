572ab52fd39d153ef59b7e1970e468081a5a27a6
Alex Sperellis <asperellis@gmail.com>
2018-12-04 09:02:49 -0500

    day 2 progress

--- day2.js
+++ day2.js
@@ -11,7 +11,7 @@ fs.readFile('inputs/day2.txt', 'utf8', (err, data) => {
         charMap[char] = 0;
       }
 
-      charMap[char]++;
+      charMap[char] += 1;
 
       return charMap;
     }, {});
@@ -36,4 +36,47 @@ fs.readFile('inputs/day2.txt', 'utf8', (err, data) => {
 
   // PART 1 Solution
   console.log(charCounts.twos * charCounts.threes);
+
+  const findSimilar = ids => {
+    const queue = ids.map(mapChars);
+
+    while (queue.length) {
+      const map1 = queue.shift();
+      for (let i = 0; i < queue.length; i++) {
+        let diffs = 0;
+        const map2 = queue[i];
+
+        for (const char in map1) {
+          diffs += Math.abs(map1[char] - (map2[char] || 0));
+        }
+
+        for (const char in map2) {
+          if (!map1[char]) {
+            diffs += map2[char];
+          }
+        }
+        if (diffs === 2) {
+          console.log([map1, map2]);
+        }
+      }
+    }
+
+    return 'no diffs';
+  };
+
+  //console.log(findSimilar(input));
+  let arr = input;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      const charsI = [...arr[i]];
+      const charsJ = [...arr[j]];
+      console.log(charsI, charsJ);
+      let diff = charsI.reduce((a, c, i) => a + (c === charsJ[i] ? 0 : 1), 0);
+
+      if (diff === 1) {
+        console.log(arr[i]);
+        console.log(arr[j]);
+      }
+    }
+  }
 });
